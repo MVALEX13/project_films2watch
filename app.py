01d4a1cd633370722ecd614d4c7c40cc5b1a4a33
Max <maxime.valle@cegetel.net>
2023-01-27 16:24:16 +0100

    deleting features

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 
-from flask import Flask,g, render_template, request
+from flask import Flask,g, render_template, request, redirect
 
 import sqlite3
 
 app = Flask(__name__)
+
+
+### FUNCTIONS ###
 
 ### return all the movies present in the database under the shape of a tab of dictionnary (1 row per movie) ###
 def get_db_data():
@@ -33,13 +36,19 @@ def clean_db_data():
     cur.execute("DELETE FROM movies;")
     conn.commit()
 
-### interrup handler executed after each http request ###
-@app.teardown_appcontext
-def close_connection(exception):
+### remove a movie from the database (remove a row of the database) ###
+def remove_movie():
+    id = request.args.get("id")
     db = getattr(g,'_database', None)
-    if db is not None:
-        db.close()
+    if db is None:
+        conn = sqlite3.connect('movies.db')
+        cur = conn.cursor()
+    cur.execute("DELETE FROM movies WHERE id = ?", id)
+    conn.commit()
 
+
+
+### VIEWS ###
 
 ### general view ###
 @app.route("/", methods = ["GET"])
@@ -55,5 +64,28 @@ def index():
 @app.route("/clean",methods = ["GET"])
 def clean():
     clean_db_data()
-    data = get_db_data()
-    return render_template("index.html",films = data)
+    return redirect("/")
+
+### removing of one movie of the list ###
+@app.route("/delete_movie", methods=["GET"])
+def delete():
+    remove_movie()
+    return redirect("/")
+
+### interrup handler executed after each http request ###
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g,'_database', None)
+    if db is not None:
+        db.close()
+
+
+### DOCUMENTATION 📚📚📚 ###
+"""
+to access the request argument with GET method => request.args.get("argument_name")
+to access the request argument with POST method => request.form.get("argument_name")
+"""
+
+
+
+
